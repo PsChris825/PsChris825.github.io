5fb530b5e80e0244bc8b4ca12cd7224a2613f59f
Chris <pillotchristopher@gmail.com>
2023-08-08 19:19:08 -0700

    updated website with Live Project screen Shots

--- final_JS.js
+++ final_JS.js
@@ -37,6 +37,71 @@ function showSlides(n) {
     dots[slideIndex - 1].className += " active"; // This adds the active styling to the dot associated with the image
 }
 
+//Second slide show
+// This function displays the first image in the slideshow when the page loads
+var slideIndex = 1;
+showSlides1(slideIndex);
+
+// This function changes the slide when the left or right arrows are clicked
+function plusSlides1(n) {
+    showSlides1(slideIndex += n);
+}
+
+// This function changes the slide when the dots are clicked
+function currentSlide1(n) {
+    showSlides1(slideIndex = n);
+}
+
+
+function showSlides1(n) {
+    var slides = document.getElementsByClassName("mySlides1"); // This takes all elements with the class name "mySlides" and stores them in the variable array "slides"
+    var dots = document.getElementsByClassName("dot1"); // This takes all elements with the class name "dot" and stores them in the variable array "dots"
+    if (n > slides.length) {slideIndex = 1}; // If n (the number passed into the function) is greater than the length of the array "slides", the slideIndex is set to 1
+    if (n < 1) {slideIndex = slides.length}; // If n (the number passed into the function) is less than 1, te slideIndex is set to the length of the array "slides"
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none"; // This for loop takes each item in the array "slides" and sets the display to none
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", ""); // This for loop takes each item in the array "dots" and removes "active" which removes the active styling
+    }
+    slides[slideIndex - 1].style.display = "block"; // This displays the image in the slideshow
+    dots[slideIndex - 1].className += " active"; // This adds the active styling to the dot associated with the image
+}
+//End of second slide show
+
+//third slide show
+// This function displays the first image in the slideshow when the page loads
+var slideIndex = 1;
+showSlides2(slideIndex);
+
+// This function changes the slide when the left or right arrows are clicked
+function plusSlides2(n) {
+    showSlides2(slideIndex += n);
+}
+
+// This function changes the slide when the dots are clicked
+function currentSlide2(n) {
+    showSlides2(slideIndex = n);
+}
+
+
+function showSlides2(n) {
+    var slides = document.getElementsByClassName("mySlides2"); // This takes all elements with the class name "mySlides" and stores them in the variable array "slides"
+    var dots = document.getElementsByClassName("dot2"); // This takes all elements with the class name "dot" and stores them in the variable array "dots"
+    if (n > slides.length) {slideIndex = 1}; // If n (the number passed into the function) is greater than the length of the array "slides", the slideIndex is set to 1
+    if (n < 1) {slideIndex = slides.length}; // If n (the number passed into the function) is less than 1, te slideIndex is set to the length of the array "slides"
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none"; // This for loop takes each item in the array "slides" and sets the display to none
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", ""); // This for loop takes each item in the array "dots" and removes "active" which removes the active styling
+    }
+    slides[slideIndex - 1].style.display = "block"; // This displays the image in the slideshow
+    dots[slideIndex - 1].className += " active"; // This adds the active styling to the dot associated with the image
+}
+
+
+
 // This code will create close the contact form when the user clicks off of it
 // The first step is to add an event listener for any clicks on the website
 document.addEventListener("click", function(event){
